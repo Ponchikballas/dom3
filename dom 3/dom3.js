613efdbabebbe9a9f095604a4845502e88fc4216
Boris Prokuror <122551971+Ponchikballas@users.noreply.github.com>
2023-01-24 11:47:44 +0300

    Add files via upload

---
+++ dom 3/dom3.js
@@ -0,0 +1,95 @@
+// // 250 задание 2
+
+// let elem = document.querySelector('#elem');
+// let elem2 = document.querySelector('#elem2');
+// let button = document.querySelector('#button');
+// button.addEventListener('click', function(){
+    
+// elem2.innerHTML = elem.value;
+// })
+
+// // 250 задание 3
+// let elem = document.querySelector('#elem');
+// let elem2 = document.querySelector('#elem2');
+// let button = document.querySelector('#button');
+// button.addEventListener('click', function (){
+//     console.log(elem2.value = (elem.value) * (elem.value));
+// })
+
+
+// // 250 задание 4
+// let elem = document.querySelector('#elem');
+// let elem2 = document.querySelector('#elem2');
+// let button = document.querySelector('#button');
+// button.addEventListener('click', function(){
+//     let a = elem.value;
+//     let b = elem2.value;
+//     elem.value = b;
+//     elem2.value = a
+// });
+ 
+
+// // 250 задание 5
+// let elem1 = document.querySelector('#elem1');
+// let elem2 = document.querySelector('#elem2');
+// let elem3 = document.querySelector('#elem3');
+// let elem4 = document.querySelector('#elem4');
+// let elem5 = document.querySelector('#elem5');
+// let button = document.querySelector('#button');
+// let text = document.querySelector('#text');
+// button.addEventListener('click', function(){
+//     text.textContent = ((Number(elem1.value) +Number(elem2.value)+ Number(elem3.value)+Number(elem4.value) + Number(elem5.value)) /5);
+// })
+
+
+// // 251 задание 1
+// let elem = document.querySelector('#elem');
+// elem.addEventListener('focus', function() {
+// 	console.log(1);
+// });
+// elem.addEventListener('blur', function() {
+// 	console.log(2);
+// });
+
+
+// // 251 задание 2
+// let elem = document.querySelector('#elem');
+// elem.addEventListener('blur', function() {
+//     console.log(elem.value = elem.value * elem.value);
+// });
+
+
+// // 251 задание 3
+// let elem = document.querySelector('#elem');
+// elem.addEventListener('blur', function() {
+//     elem.value = "";
+// })
+
+
+
+
+// // 252 задание 1
+// let elem = document.querySelector('#elem');
+// let button = document.querySelector('#button');
+// button.addEventListener('click', function(){
+//     console.log(elem.className);
+// })
+  
+
+// // 252 задание 2
+// let elem = document.querySelector('#elem');
+// let button = document.querySelector('#button');
+// button.addEventListener('click', function(){
+//     console.log(elem.className = "changed class");
+// })
+
+
+// // 252 задание 3
+// let elem = document.querySelector('#elem');
+// let button = document.querySelector('#button');
+// button.addEventListener('click', function(){
+// let a = elem.className;
+// let arr = a.split('');
+// console.log(arr);
+// })
+    
